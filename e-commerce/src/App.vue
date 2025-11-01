ae38605872dd4b98efe73bee7cd009b03059cea5
Hong Dyhengmesa <mesahongdyheng@gmail.com>
2025-11-02 00:46:56 +0700

    This is for exercise4

--- e-commerce/src/App.vue
+++ e-commerce/src/App.vue
@@ -1,73 +1,156 @@
-<script setup lang="ts">
-const message = 'This is my first VueJs Project'
-</script>
-
 <template>
-  <div>
-    <h1>{{ message }}</h1>
-  </div>
+
+    <div class="card-design">
+
+      <div class="tamplate-card">
+        <div v-for="(product, index) in products" :key="index">
+            <SmallCard  :title="product.title" :desc="product.desc" :image="product.image" :bgcolor="product.bgcolor"/>
+        </div>
+      </div>
+
+    <div class="big-template">
+      <div v-for="(bigproduct,index) in bigproducts" :key="index">
+        <BigCard :titlebig="bigproduct.titlebig" :button="bigproduct.button" :bigcolor="bigproduct.bigcolor" :bigimg="bigproduct.bigimg" :btncolor="bigproduct.btncolor"/>
+      </div>
+    </div>
+
+    </div>
+
 </template>
 
+<script lang="ts">
+import { reactive } from 'vue';
+import SmallCard from './components/SmallCard.vue';
+import BigCard from './components/BigCard.vue';
+
+
+    export default{
+        components: { SmallCard, BigCard },
+        name : 'app',
+        data(){
+          return{
+            products : reactive([
+              {
+                id : 1,
+                title: "Cake & Milk",
+                desc : "14items",
+                image : "./src/img/card1.png",
+                bgcolor : '#F2FCE4'
+              },
+              {
+                id : 2,
+                title: "Orange",
+                desc : "17items",
+                image : "./src/img/card2.png",
+                bgcolor : '#FFFCEB'
+              },
+              {
+                id : 3,
+                title: "Orginic Kiwi",
+                desc : "21items",
+                image : "./src/img/card3.png",
+                bgcolor : '#ECFFEC'
+              },
+              {
+                id : 4,
+                title: "Peach",
+                desc : "17items",
+                image : "./src/img/card4.png",
+                bgcolor : '#FEEFEA'
+              },
+              {
+                id :5,
+                title: "Headphone",
+                desc : "33items ",
+                image : "./src/img/card5.png",
+                bgcolor : '#FFF3EB'
+              },
+              {
+                id : 6,
+                title: "Vegetables",
+                desc : "65items",
+                image : "./src/img/card6.png",
+                bgcolor : '#FFF3FF'
+              },
+              {
+                id : 7,
+                title: "Black plum",
+                desc : "25items",
+                image : "./src/img/card7.png",
+                bgcolor : '#F2FCE4'
+              },
+              {
+                id: 8,
+                title: "Red Apple",
+                desc : "68items",
+                image : "./src/img/card8.png",
+                bgcolor : '#FFFCEB'
+              },
+              {
+                id : 9,
+                title: "Cake & Milk",
+                desc : "54items",
+                image : "./src/img/card9.png",
+                bgcolor : '#F2FCE4'
+              },
+              {
+                id : 10,
+                title: "Snack",
+                desc : "34items",
+                image : "./src/img/card10.png",
+                bgcolor : '#FFF3FF'
+              }
+            ]),
+            bigproducts : reactive([
+            {
+              id : 1,
+              titlebig : "Everyday Free & Clean with Our Products ",
+              button : "Shop Now",
+              bigimg : "./src/img/big-card3.jpg",
+              btncolor: '#3BB77E',
+              bigcolor: '#F0E8D5'
+            },
+            {
+              id : 2,
+              titlebig : "Make your Breakfast Healthy and Easy",
+              button : "Shop Now",
+              bigimg : "./src/img/big-card2.png",
+              btncolor: '#3BB77E',
+              bigcolor: '#F3E8E8'
+            },
+            {
+              id : 3,
+              titlebig : "The best Organic Products Online",
+              button : "Shop Now",
+              bigimg : "./src/img/big-card1.jpg",
+              btncolor: '#FDC040',
+              bigcolor: '#E7EAF3'
+            },
+          ])
+          }
+        },
+    }
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+    body{
+      width: 100%;
+      height: 100vh;
+    }
+    .tamplate-card{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+        margin-bottom: 30px;
+    }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+    .big-template{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-left: 300px;
+      margin-right: 300px;
+      gap: 30px;
+    }
 </style>
---
+++ e-commerce/src/components/BigCard.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="bigcard-container" :style="{backgroundColor: bigcolor}">
+        <div class="main-big">
+            <div class="title-left">
+                <h2>{{ titlebig }}</h2>
+                <button class="big-btn" :style="{backgroundColor: btncolor}">{{ button }} →</button>
+            </div>
+            <div class="img-right">
+                <img :src="bigimg" class="big-img" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    export default{
+        name : 'BigCard',
+        props :{
+            titlebig :{
+                type : String,
+                required : true
+            },
+            button :{
+                type : String,
+                requeired : true
+            },
+            bigcolor :{
+                type : String,
+                requeired : true
+            },
+            bigimg :{
+                type: String,
+                requeired: true
+            },
+            btncolor :{
+                type : String,
+                requeired : true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .bigcard-container{
+        width: 100%;
+        height: 100vh;
+    }
+
+    .main-big{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .bigcard-container{
+        width: 512px;
+        height: 300px;
+        border-radius: 10px;
+        transition: transform 0.4s;
+    }
+
+    .bigcard-container:hover{
+        transform: scale(1.1);
+        border: 1px solid gray;
+        box-shadow: 3px 0px 3px 0px rgb(122, 119, 119);
+    }
+
+    .bigcard-container .big-img{
+        width: 204px ;
+        height: 216px;
+        margin: 70px 40px 20px 10px;
+    }
+
+    .bigcard-container .big-btn{
+        width: 110px;
+        height: 32px;
+        border: none;
+        color: white;
+        font-size: 15px;
+        font-weight: 600;
+        border-radius: 5px;
+        margin-left: 40px;
+        margin-top: 10px;
+    }
+
+    h2{
+        margin-left: 40px;
+        font-size: 16px;
+        font-weight: 600;
+    }
+
+
+</style>
